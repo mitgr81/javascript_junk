76ab0617697b5fb439d3eabfb67b2e7a67575a8c
Chris McGraw <chris.mcgraw@lodgenet.com>
2012-08-27 22:23:39 -0500

    Fleshes out the lettercaps

---
+++ static/js/lettercaps.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('input').on('keyup', function(event){
+        var text = $(event.target).val(),
+            $echo = $('#echo');
+
+        $('#echo').html(text);
+        if(text === ''){
+            $echo.fadeOut();
+        }else{
+            $echo.empty();
+            for (var textchar in text) {
+                var my_character = text.charAt(textchar),
+                    $img = $('<img>');
+                $img.attr('src', 'static/images/lettercaps_letters/' + my_character + '.jpg');
+                $img.attr('alt', my_character);
+                $img.attr('title', my_character);
+                $echo.append($img);
+            }
+            $echo.fadeIn();
+        }
+    });
+});
